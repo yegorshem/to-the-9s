80437c0268a9db772dea3e093d640ada0147d084
Andrew Dickinson <andrew.dickinson@tee2.org>
2017-11-14 12:13:46 -0800

    created a form with a success

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ const About = () =>{
       	<div id="blkText">
         	<p>To The Nines Chauffeured Services has built a first class reputation in the Willamette Valley, and we continue to strive for excellence and higher standards for our customers. We understand that discerning clients deserve to be catered to in a manner which reflects both superior quality of service and discretion. If, for any reason, we fail to live up to the expectations of any guest, we want to hear about it. To The Nines Chauffeured Services will do its best to rectify the situation to everyone's satisfaction.</p>
       	</div>   
+      	
       </div>
     )
 } 
