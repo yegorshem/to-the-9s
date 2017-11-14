ccafc470ef02af01beef75eb70e246a9c62f1cb0
Andrew <acdickin@gmail.com>
2017-11-13 20:20:49 -0800

    updated carosel and Reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -3,8 +3,15 @@ import React from 'react'
 const Reviews =()=>{
   return(
       <div id="reviews" className="content">
-          <h2>Reviews</h2>
+
+          <h3>Tell us about your experience...</h3>
+
+            <p> "Thank you so much for making the end of our special night so memorable.  You were absolutely amazing and so accommodating.  Your customer service couldn't have been better.  The limo was beautiful and the seats were so comfortable.  The kids had so much fun being in a limo for the first time!  Thank you, thank you so incredibly much.  We definitely will refer you if anyone we know needs a limo."     - Melyssa</p>
+
+            <p>"Our experience with you was perfect, to say the least.  You are a very professional and warm hearted person that put up with a huge party of people with a lot of demands.  You were there with an umbrella when the rain surprised us and a calm face and smile for me to focus on when things were getting overwhelming.  The service was amazing and we didn't feel rushed at all.  Thank you for taking your time, for having patience and for being the cherry on top of our ice cream on a very intense day that turned out absolutely beautiful and perfect."     -Oana</p>
+
+
       </div>
-  )  
-} 
+  )
+}
 export default Reviews;
