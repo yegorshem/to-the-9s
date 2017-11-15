a0083153ac9e1537f7fddda8d8040cead144bb52
Andrew Dickinson <andrew.dickinson@tee2.org>
2017-11-15 11:52:25 -0800

    Updated prod to use scss and updated header

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -166,6 +166,42 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
+            test: /\.scss$/,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: "style-loader"
+              }, {
+                loader: "css-loader"
+              }, {
+                loader: "sass-loader",
+                options: {
+                  includePaths: ["absolute/path/a", "absolute/path/b"]
+                }
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+            ],
+          },
+          {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,42 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 const logo= require('../img/logo.jpg')
-const menu = require('../img/menu.svg')
+
 class Header extends React.Component {
  constructor() {
     super();
-    this.state = { showMenu:false } ;
+    this.state = { menuOpen:false } ;
   }
   toggleDropdown = () =>{
-    this.setState({showMenu: !this.state.showMenu});
+    this.setState({menuOpen: !this.state.menuOpen});
   }
   render(){
-    console.log(this.state.showMenu)
+    console.log(this.state.menuOpen)
     return(
       <div id="header">
         <div id="logo">
           <img src={logo} alt="To the nines logo"/> 
         </div>
-        <img id="menu" onClick={this.toggleDropdown} src={menu} alt="menu"/>
-        <div id="links" className={(this.state.showMenu)? "active" : "" }>
-          <NavLink exact to='/' activeClassName="active">Home</NavLink>
-          <NavLink to ='/about' activeClassName="active">About us</NavLink>
-          <NavLink to='/reviews' activeClassName="active">Reviews</NavLink>
-          <NavLink to ='/events' activeClassName="active">Events</NavLink>
-          <NavLink to='/reservation' activeClassName="active">Reservation</NavLink>
+        
+        <div id="menu" onClick={this.toggleDropdown} className={(this.state.menuOpen)? "open" : ""}>
+          <div id="nav-icon" onClick={this.toggleDropdown} className={(this.state.menuOpen)? "open" : ""}>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+          </div>
+          <div id="links" className={(this.state.menuOpen)? "active" : "hidden" }>
+            <NavLink exact to='/' activeClassName="active">Home</NavLink>
+            <NavLink to ='/about' activeClassName="active">About us</NavLink>
+            <NavLink to='/reviews' activeClassName="active">Reviews</NavLink>
+            <NavLink to ='/events' activeClassName="active">Events</NavLink>
+            <NavLink to='/reservation' activeClassName="active">Reservation</NavLink>
+          </div>
         </div>
       </div>
+
     )   
   }
 } 
